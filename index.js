f5a1a3eec8c8309e5505e7a5235d012f5325e723
Edwin Neoh <edwinneoh@gmail.com>
2025-11-11 10:50:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-// Enable CORS
 app.use(cors());
 
-// --- This is the only part you need to serve your site ---
-// It tells Express to serve all files from the 'public' folder
-// and automatically find 'index.html' when someone visits '/'
+// This serves all static files in your 'public' folder
+// (like .css, .js, and images)
 app.use(express.static(path.join(__dirname, 'public')));
+
+// This explicitly tells the server to send 'index.html'
+// when someone visits the main root URL ('/')
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 // --- Your API routes will go here later ---
 // app.post('/api/estimate', (req, res) => {
