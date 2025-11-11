1a48a06b539c63dc26101dcfbd64c9bbc43261f5
Edwin Neoh <edwinneoh@gmail.com>
2025-11-11 22:06:52 +0800

    Login page only

---
+++ public/js/firebase-config.js
@@ -0,0 +1,10 @@
+// This object contains your project's unique Firebase keys.
+// Replace this with your actual config object from the Firebase console!
+window.__firebase_config = {
+  apiKey: "AIzaSy...YOUR_KEY...",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456"
+};
