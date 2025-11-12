a784a1eb88e3ca43083dbadcb64ca9123abe34fc
Edwin Neoh <edwinneoh@gmail.com>
2025-11-13 00:54:00 +0800

    Profileview.js (phone number) update

--- public/js/views/profileView.js
+++ public/js/views/profileView.js
@@ -58,6 +58,7 @@ export async function renderProfile(container) {
   // 4. Set the data variables from the fetched user
   const userName = user.username;
   const userEmail = user.email;
+  const userPhone = user.phone_number || '';
   const userInitial = userName.charAt(0).toUpperCase();
 
   // 5. Dummy valuation data (we will make this real later)
@@ -161,7 +162,7 @@ export async function renderProfile(container) {
                           <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.63A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"/>
                         </svg>
                       </span>
-                      <input id="phone" type="tel" class="form-input with-icon" value="+60 12-345 6789" placeholder="Add phone number">
+                      <input id="phone" type="tel" class="form-input with-icon" value="${userPhone}" placeholder="Add phone number">
                     </div>
                   </div>
                   
