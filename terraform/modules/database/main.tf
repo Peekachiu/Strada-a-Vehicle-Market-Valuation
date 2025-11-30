970ce991fa0d03d72128e645aa692958da6b9e1c
Edwin Neoh <edwinneoh@gmail.com>
2025-11-30 21:51:08 +0800

    Terraform Updates

--- terraform/modules/database/main.tf
+++ terraform/modules/database/main.tf
@@ -10,7 +10,7 @@ resource "aws_db_subnet_group" "main" {
 resource "aws_db_instance" "main" {
   identifier        = "${var.project_name}-db"
   engine            = "postgres"
-  engine_version    = "16.3" # Check for latest supported version
+  engine_version    = "16.1" # Check for latest supported version
   instance_class    = "db.t3.micro"
   allocated_storage = 20
   storage_type      = "gp2"
