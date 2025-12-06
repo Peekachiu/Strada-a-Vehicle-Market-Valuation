dcbc5f692b7888654177c322054160da9dfeccb3
Edwin Neoh <edwinneoh@gmail.com>
2025-12-06 14:44:34 +0800

    Provider Config

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,11 @@ provider "aws" {
   region = var.region
 }
 
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
 module "vpc" {
   source       = "./modules/vpc"
   project_name = var.project_name
