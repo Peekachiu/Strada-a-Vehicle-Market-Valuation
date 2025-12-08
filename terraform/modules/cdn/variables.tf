b77ceb9dad7394243ee2c0ec87237286388bf59a
Edwin Neoh <edwinneoh@gmail.com>
2025-12-08 14:58:40 +0800

    Update variables.tf

--- terraform/modules/cdn/variables.tf
+++ terraform/modules/cdn/variables.tf
@@ -7,3 +7,9 @@ variable "alb_dns_name" {
   description = "DNS name of the Public ALB"
   type        = string
 }
+
+variable "web_acl_id" {
+  description = "ARN of the WAF Web ACL"
+  type        = string
+  default     = "" # Optional if you want to deploy without WAF initially, or make it required.
+}
