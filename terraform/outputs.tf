3c5fa5144a913168bbe207bb03d162dc568d16e9
Edwin Neoh <edwinneoh@gmail.com>
2025-12-13 16:54:45 +0800

    TF Unlock

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,11 @@
-output "nameservers" {
-  value = module.route53.name_servers
-}
-
 output "alb_dns_name" {
   value = module.public_alb.alb_dns_name
+}
+
+output "rds_endpoint" {
+  value = module.database.db_endpoint
+}
+
+output "nameservers" {
+  value = module.route53.name_servers
 }
