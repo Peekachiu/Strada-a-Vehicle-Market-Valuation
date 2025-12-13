f03494d948f95225beb0fb433203f6e0f7c5cd9c
Edwin Neoh <edwinneoh@gmail.com>
2025-12-14 00:16:06 +0800

    Update variables.tf

--- terraform/modules/cdn/variables.tf
+++ terraform/modules/cdn/variables.tf
@@ -24,3 +24,8 @@ variable "aliases" {
   type        = list(string)
   default     = []
 }
+
+variable "s3_bucket_domain_name" {
+  description = "Regional domain name of the S3 bucket"
+  type        = string
+}
